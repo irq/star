a17e8c8827ae8b19978a44a3f1978d03edf45983
Andreas Sion <asi@trustpilot.com>
2015-11-24 20:18:45 +0100

    Placed "Player" at the bottom center of the game

--- app/scripts/states/GameBoard.ts
+++ app/scripts/states/GameBoard.ts
@@ -9,7 +9,7 @@ namespace Star {
             this.physics.startSystem(Phaser.Physics.ARCADE);
             this.music = this.add.audio('music', 1, false);
             this.music.play();
-            this.player = new Player(this.game, 500, 500);
+            this.player = new Player(this.game, this.world.width / 2, this.world.height - 50);
             this.game.stage.backgroundColor = 0xffffff;
         }
     }
